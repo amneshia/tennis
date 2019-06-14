b5fd2f666e7ccc18056d5dc0b8f2b45fc3ebc8c8
amneshia <amirshayan.armaghan@gmail.com>
2019-06-14 18:50:22 +0430

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,5 @@ import           Tennis        (tennisMatchPretty)
 
 main :: IO ()
 main = do
-  _ <- putStrLn "Enter the 1st Player's name:"
-  p1Name <- getNameWithFallback "Nadal"
-  _ <- putStrLn "Enter the 2st Player's name:"
   stdGen <- newStdGen
-  p2Name <- getNameWithFallback "Federer"
-  putStrLn $ tennisMatchPretty stdGen p1Name p2Name
-  where
-    getNameWithFallback fallback = do
-      n <- getLine
-      case sanitize n of
-        [] -> do
-          _ <- putStrLn $ "No name entered. taking \'" <> fallback <> "\' as fallback."
-          return fallback
-        n' -> return n'
-      where
-        sanitize = take 15 . filter (/= '\n') . filter (/= ' ')
+  putStrLn $ tennisMatchPretty stdGen "Nadal" "Federer"
